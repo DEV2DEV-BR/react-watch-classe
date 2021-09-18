4046d4d270f2eb632a3155306cee78ada3798d67
Daniel <danieldeandradelopes@gmail.com>
2021-09-18 09:08:19 -0300

    create boilerplate

---
+++ src/components/CardClasse/index.js
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+export function CardClasse() {
+  return (
+    <h1>Test</h1>
+  );
+}
---
+++ src/components/WatchClasse/index.js
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+export function WatchClasse() {
+  return (
+    <h1>Test</h1>
+  );
+}
