87113eb0ffaf5818b78f3eed7c86a076113a5acc
Daniel <danieldeandradelopes@gmail.com>
2021-09-18 10:08:35 -0300

    add mock of data in Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,48 +3,60 @@ import { WatchClasse } from '../../components/WatchClasse'
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { CardClasse } from '../../components/CardClasse';
+import { useState } from 'react';
 
 const classesList = [
   {
     id: 1,
     cover: "https://www.luiztools.com.br/wp-content/uploads/2020/06/reactJS.png",
     title: "1. Criando o projeto",
+    url: "https://www.youtube.com/watch?v=kolNylRoIrc",
+    description: "Descrição da primeira aula",
     links: [
-      {
-        title: "Link",
-        url: ""
-      }
+      { id: 1, url: "https://www.youtube.com/watch?v=kolNylRoIrc", title: "Título do link 1", },
+      { id: 2, url: "https://www.youtube.com/watch?v=kolNylRoIrc", title: "Título do link 2", }
     ]
   },
   {
     id: 2,
     cover: "https://css-tricks.com/wp-content/uploads/2018/04/react.jpg",
     title: "2. Configurando o css",
+    url: "https://www.youtube.com/watch?v=ilLwqcgJF4E&t=36s",
+    description: "Descrição da primeira aula",
     links: [
-      {
-        title: "Link",
-        url: ""
-      }
+      { id: 1, url: "https://www.youtube.com/watch?v=kolNylRoIrc", title: "Título do link 1", },
+      { id: 2, url: "https://www.youtube.com/watch?v=kolNylRoIrc", title: "Título do link 2", }
     ]
   },
   {
     id: 3,
     cover: "https://programadoresbrasil.com.br/wp-content/uploads/2020/07/reactjs-logo-1-scaled.jpg",
     title: "3. Criando o component Watch Classe",
+    url: "https://www.youtube.com/watch?v=Xg1jD1AH3gQ&t=1161s",
+    description: "Descrição da primeira aula",
     links: [
-      {
-        title: "Link",
-        url: ""
-      }
+      { id: 1, url: "https://www.youtube.com/watch?v=kolNylRoIrc", title: "Título do link 1", },
+      { id: 2, url: "https://www.youtube.com/watch?v=kolNylRoIrc", title: "Título do link 2", }
     ]
   }
 ]
 
 export function Main() {
 
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [currentClasse, setCurrentClasse] = useState({
+    id: 0,
+    cover: '',
+    url: '',
+    description: '',
+    title: '',
+    links: []
+  })
 
-  function openModal() {
+  function openModal(classeId) {
+    const item = classesList.find(classe => classe.id === classeId)
+    setCurrentClasse(item);
+
     setIsOpen(true);
   }
 
@@ -57,16 +69,25 @@ export function Main() {
       <div className={styles.container}>
         <Header />
         <main className={styles.main}>
-          <CardClasse />
+          {classesList.map(classe => (
+            <CardClasse
+              key={classe.id}
+              classeId={classe.id}
+              cover={classe.cover}
+              title={classe.title}
+              openModal={openModal}
+            />
+          ))}
+
         </main>
         <Footer />
         <WatchClasse
-          closeModal={closeModal}
+          videoUrl={currentClasse.url}
+          nameClasse={currentClasse.title}
+          description={currentClasse.description}
           modalIsOpen={modalIsOpen}
-          videoUrl=""
-          nameClasse=""
-          description=""
-          links={ }
+          closeModal={closeModal}
+          links={currentClasse.links}
         />
       </div>
     </>
